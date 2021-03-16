e956fb9915ffbd3c56ff87704b7db96f3b64353f
tbmun21 <macbook@Macbook-MacBook-Pro.local>
2021-03-16 11:25:08 +0300

    tbmun
    initial commit

---
+++ js/custom.js
@@ -0,0 +1,89 @@
+(function($) {
+
+    // Init Wow
+    wow = new WOW({
+        animateClass: 'animated',
+        offset: 100
+    });
+    wow.init();
+
+    $(".navbar-collapse a").on('click', function() {
+        $(".navbar-collapse.collapse").removeClass('in');
+    });
+
+    // Navigation scrolls
+    $('.navbar-nav li a').bind('click', function(event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+
+            event.preventDefault();
+        }
+    });
+    // countdown
+    (function() {
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        let birthday = "Apr 16, 2021 00:00:00",
+            countDown = new Date(birthday).getTime(),
+            x = setInterval(function() {
+
+                let now = new Date().getTime(),
+                    distance = countDown - now;
+
+                document.getElementById("days").innerText = Math.floor(distance / (day)),
+                    document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                    document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                    document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+                //do something later when date is reached
+                if (distance < 0) {
+                    let headline = document.getElementById("headline"),
+                        countdown = document.getElementById("countdown"),
+                        content = document.getElementById("content");
+
+                    headline.innerText = "It's my birthday!";
+                    countdown.style.display = "none";
+                    content.style.display = "block";
+
+                    clearInterval(x);
+                }
+                //seconds
+            }, 0)
+    }());
+
+    // About section scroll
+    $(".overlay-detail a").on('click', function(event) {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 900, function() {
+            window.location.hash = hash;
+        });
+    });
+
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar-default").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    // Testimonials Slider
+    $('.bxslider').bxSlider({
+        adaptiveHeight: true,
+        mode: 'fade'
+    });
+
+})(jQuery);
